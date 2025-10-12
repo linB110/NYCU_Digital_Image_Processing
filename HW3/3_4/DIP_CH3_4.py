25c5350d3fb62e23cfa46ab45ef40c07e0b03d2d
Lin Huang Ting <138973404+linB110@users.noreply.github.com>
2025-10-12 20:46:14 +0800

    Update DIP_CH3_4.py

--- HW3/3_4/DIP_CH3_4.py
+++ HW3/3_4/DIP_CH3_4.py
@@ -37,7 +37,6 @@ def sobel_filtering(input_image, filter_size=3, robust_norm=True):
         for j in range(w):
             output_image[i, j] = np.sqrt( edge_x[i, j]**2 + edge_y[i, j]**2 )
     
-    print(max(output_image[:,:]), min(output_image[:,:]))   
     # avoid pixel value distortion by extremes        
     if robust_norm:
         p1, p99 = np.percentile(output_image, (1, 99))
@@ -48,36 +47,52 @@ def sobel_filtering(input_image, filter_size=3, robust_norm=True):
         
     return output_image.astype(np.uint8)
 
-#def Laplacian_filtering(input_image):
+def Laplacian_filtering(input_image):
+    laplace_kernel = np.array([[-1,-1,-1], [-1,8,-1], [-1,-1,-1]])
+    
+    h, w = input_image.shape
+    output_image = np.zeros((h, w))
+    
+    output_image = convolve2d(input_image, laplace_kernel, mode='same', boundary='symm')
+    
+    p1, p99 = np.percentile(output_image, (1, 99))
+    scale = max(p99 - p1, 1e-9)
+    output_image = np.clip((output_image - p1) / scale, 0, 1) * 255
+    
+    return  input_image + output_image
 
 # read image
 image_path = "Bodybone.bmp"
+#image_path = "fish.jpg"
+
 with rasterio.open(image_path) as raw_img:
     #print(src.count)     # see how many bands are there
     image = raw_img.read(1)   # grayscale image => only one band 
         
 sobel_image = sobel_filtering(image, robust_norm=False)    
 enhanced_sobel = sobel_filtering(image, robust_norm=True)
+laplacian_image = Laplacian_filtering(image)
 
-plt.figure(figsize=(15,4))
-# plt.subplot(1,2,1)
-# plt.imshow(image, cmap='gray')
-# plt.title('Original')
-# plt.axis('off')
-
-plt.subplot(1,3,1)
+plt.figure(figsize=(20,4))
+plt.subplot(1,4,1)
 plt.imshow(image, cmap='gray')
 plt.title('Original Image')
 plt.axis('off')
 
-plt.subplot(1,3,2)
+plt.subplot(1,4,2)
 plt.imshow(sobel_image, cmap='gray')
 plt.title('Sobel filter')
 plt.axis('off')
 
-plt.subplot(1,3,3)
+plt.subplot(1,4,3)
 plt.imshow(enhanced_sobel, cmap='gray')
 plt.title('Enhanced Sobel ')
 plt.axis('off')
 
+plt.subplot(1,4,4)
+plt.imshow(laplacian_image, cmap='gray')
+plt.title('Laplacian Image')
+plt.axis('off')
 plt.show()
+
+
